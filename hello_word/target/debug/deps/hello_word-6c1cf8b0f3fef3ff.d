7247226fb91c44710d7ec542b5b5e7ed9dbc35df
Pedro Marinho <pedro.marinho238@gmail.com>
2020-06-14 12:56:04 -0400

    gitignore

--- hello_word/target/debug/deps/hello_word-6c1cf8b0f3fef3ff.d
+++
@@ -1,5 +0,0 @@
-/home/pc/projects/aula_rust/hello_word/target/debug/deps/hello_word-6c1cf8b0f3fef3ff: src/main.rs
-
-/home/pc/projects/aula_rust/hello_word/target/debug/deps/hello_word-6c1cf8b0f3fef3ff.d: src/main.rs
-
-src/main.rs:
--- hello_word/target/debug/hello_word.d
+++
@@ -1,1 +0,0 @@
-/home/pc/projects/aula_rust/hello_word/target/debug/hello_word: /home/pc/projects/aula_rust/hello_word/src/main.rs
