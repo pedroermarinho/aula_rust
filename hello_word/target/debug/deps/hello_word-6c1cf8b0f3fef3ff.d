c046559d1b4b60268a06503f3501fd1433f5673b
Pedro Marinho <pedro.marinho238@gmail.com>
2020-06-14 12:47:42 -0400

    first commit

---
+++ hello_word/target/debug/deps/hello_word-6c1cf8b0f3fef3ff.d
@@ -0,0 +1,5 @@
+/home/pc/projects/aula_rust/hello_word/target/debug/deps/hello_word-6c1cf8b0f3fef3ff: src/main.rs
+
+/home/pc/projects/aula_rust/hello_word/target/debug/deps/hello_word-6c1cf8b0f3fef3ff.d: src/main.rs
+
+src/main.rs:
---
+++ hello_word/target/debug/hello_word.d
@@ -0,0 +1,1 @@
+/home/pc/projects/aula_rust/hello_word/target/debug/hello_word: /home/pc/projects/aula_rust/hello_word/src/main.rs
